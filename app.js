89efff664754f7526e3b6e6b3767fb1769cfcf80
Jakub Mokosa <jakub.mokosa@pl.cdprojekt.com>
2017-08-16 15:33:17 +0200

    Remove magic strings and replace them with config module

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var Emitter = require('./ori_emitter.js');
+var config = require('./config.js')
 
 var emitter1 = new Emitter();
 
-emitter1.on('file_saved', function () {
+emitter1.on(config.events.FILE_SAVED, function () {
     console.log('File has been saved.');
 });
 
-emitter1.on('file_saved', function () {
+emitter1.on(config.events.FILE_SAVED, function () {
     console.log('Another listener for file_saved event has been executed.');
 });
 
-emitter1.emit('file_saved');
+emitter1.emit(config.events.FILE_SAVED);
---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    events: {
+        FILE_SAVED: 'file_saved'
+    }
+}
